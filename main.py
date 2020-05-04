ca453008ad507dfae9ce95da22b8b0f6c9df0402
Etxau24 <96jonetxauri@gmail.com>
2020-05-04 14:34:42 +0200

    Sorkuntza

---
+++ main.py
@@ -0,0 +1,14 @@
+# 
+# main.c
+# 
+# Main program file, first one to be executed.
+# Created: May 2020
+#
+
+def main():
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
+
+print("Value in built variable name is:  ",__name__)
